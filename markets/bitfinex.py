54279668cd90f68d5d8d3935bfc02f0ec507490f
hydrogenesis <hdg@gmx.com>
2016-08-13 14:50:06 +0000

    lend

--- markets/bitfinex.py
+++ markets/bitfinex.py
@@ -221,8 +221,8 @@ def auto_renew(bitfinex, max_ask = 50000):
   elif target_rate > 73:
     days = 30
   if target_rate < 8:
-    print "Target rate too low. I'd rather wait: %.02f%%" % target_rate
-    return
+    print "Target rate too low. I'd not offer: %.02f%%" % target_rate
+    target_rate = 8
   print '***** Lending out %f usd at yearly rate %f for %d days ******' %(lend_funds, target_rate, days)
   print bitfinex.new_offer(amount = lend_funds, rate = target_rate, period = days)
   print '***** Offers ******'
